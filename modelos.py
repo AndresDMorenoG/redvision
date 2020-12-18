fdfe66ae13d4ed34a36eebaa71efe5e5dc5b3948
Oscar Bustillo <oscardavidbustillo@gmail.com>
2020-12-17 20:00:54 -0500

    mvc
    se arreglo problemas de ejecucion del servidor por medio de python, utilizando el mvc

--- modelos.py
+++ modelos.py
@@ -1,4 +1,4 @@
-from app import db, app
+from app import db
 from datetime import datetime
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 #-------------------------------------------------------------------- 
